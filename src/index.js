3783f26fff9721aeed432223fdc57ddd62281600
callblueday <hujinhong@makeblock.cc>
2018-03-24 09:47:42 +0800

    增加fabric测试实例

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-document.write("<h1>hello world</h1>");
+let fabric = require('fabric').fabric;
+
+
+var canvas = new fabric.Canvas('c', {
+  backgroundColor: 'rgb(100,100,200)',
+  selectionColor: 'blue',
+  selectionLineWidth: 2,
+  width: 500,
+  height: 400
+  // ...
+});
+
+// create a rectangle object
+var rect = new fabric.Rect({
+  left: 100,
+  top: 100,
+  fill: 'red',
+  width: 100,
+  height: 100
+});
+
+window.canvas = canvas;
+window.rect = rect;
+
+// "add" rectangle onto canvas
+canvas.add(rect);
